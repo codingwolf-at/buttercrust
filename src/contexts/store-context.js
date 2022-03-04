2811fe18439ca21cb3f119ef9b7f962d666a932b
codingwolf-at <atameshwari9@gmail.com>
2022-03-05 00:00:39 +0530

    feat: handling state using useReducer

--- src/contexts/store-context.js
+++ src/contexts/store-context.js
@@ -1,10 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
+  
+  const storeReducer = (state, action) => {
+    switch(action.type) {
+      case "INITIALIZE_MENU":
+        return {
+          ...state,
+          menu: action.payload
+        };
+      default:
+        return state;
+    }
+  }
+  
+  const [state, dispatch] = useReducer(storeReducer, { menu: [], cart: [] });
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68");
+      dispatch({ type: "INITIALIZE_MENU", payload: data });
+    })();
+  }, []);
+
   return (
-    <StoreContext.Provider value={{}} >
+    <StoreContext.Provider value={{ menu: state.menu, cart: state.cart, dispatch }} >
       { children }
     </StoreContext.Provider>
   )
