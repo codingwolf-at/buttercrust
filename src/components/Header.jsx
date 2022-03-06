5c44cfd52b3847c4015496706bffd250ca5fdb37
codingwolf-at <atameshwari9@gmail.com>
2022-03-07 00:36:00 +0530

    fear: added button link to cart page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
-import { Typography, AppBar, Toolbar } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { Typography, AppBar, Toolbar, Button } from "@mui/material";
 
 export const Header = () => {
   return (
     <AppBar position="relative">
-      <Toolbar>
-        <Typography variant="h4">
+      <Toolbar sx = {{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <Typography variant="h4" component="h1">
           Buttercrust
         </Typography>
+        <Button href="/cart" startIcon={<ShoppingCart />} variant="outlined" sx={{ color: "white", border: "2px solid white" }}>
+          Cart
+        </Button>
       </Toolbar>
     </AppBar>
   )
